53e90695a9265404c8c9aef37cf929da822d0c7e
21givenchy <georgekarani44@mail.com>
2022-06-18 10:52:16 +0000

    wes

--- portfolio/main.js
+++ portfolio/main.js
@@ -34,7 +34,8 @@ function addStar() {
     const geometry = new THREE.SphereGeometry(0.25, 24, 24);
     const material = new THREE.MeshBasicMaterial({ color: 0xffffff })
     const star = new THREE.Mesh(geometry, material);
-    const [x, y, z] = Array(3).fill().map(() => Math.random() * 100 - 50);
+    const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+    star.position.set(x, y, z);
     scene.add(star);
 }
 
