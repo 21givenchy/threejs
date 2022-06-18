b668851af7c6319a96201ef2b87edf6d8ba25d51
21givenchy <georgekarani44@mail.com>
2022-06-18 10:32:11 +0000

    is

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,5 +1,10 @@
 import './style.css'
 import * as THREE from 'three';
+import { orbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+
+
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({
@@ -14,7 +19,27 @@ renderer.render(scene, camera);
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
 const material = new THREE.MeshBasicMaterial({ color: 0x00FF6347, wireframe: true });
 const torus = new THREE.Mesh(geometry, material);
+
 scene.add(torus)
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(pointLight, ambientLight);
+const lightHelper = new THREE.PointLightHelper(pointLight)
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper);
+const controls = new orbitControls(camera, renderer.domElement);
+
+function addStar() {
+    const geometry = new THREE.SphereGeometry(0.5, 32, 32);
+    const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+    const star = new THREE.Mesh(geometry, material);
+    star.position.set(Math.random() * 100 - 50, Math.random() * 100 - 50, Math.random() * 100 - 50);
+    scene.add(star);
+}
+
+Array(200).fill(0).forEach(addStar);
+
 
 function animate() {
     requestAnimationFrame(animate);
@@ -22,6 +47,8 @@ function animate() {
 
     torus.rotation.x += 0.01;
     torus.rotation.y += 0.01;
+    torus.rotation.z += 0.001;
+    controls.update();
 
     renderer.render(scene, camera);
 }
